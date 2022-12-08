992f2044c18665e31c09b3c09a1490c6d6d99a57
Renzo R. Gelmetti <renzo.gelmetti@seznam.cz>
2022-12-08 20:47:25 +0100

    object with questions moved to main.js

--- main.js
+++ main.js
@@ -36,3 +36,62 @@ function restartGame() {
     message.innerHTML = ''
     button.remove()
 }
+
+export const DB = [
+    {
+    question: "Kdo vyhrál MS v roce 2006?",
+    "correct answer": 'Itálie',
+    A: 'Francie',
+    B: 'Itálie',
+    C: 'Anglie',
+    D: 'Argentina'
+},
+    {
+    question: "Kde se hrálo MS v roce 2002?",
+    "correct answer": 'V Japonsku a Koreji',
+    A: 'V Brazílii',
+    B: 'V USA',
+    C: 'V Japonsku a Koreji',
+    D: 'V Itálii'
+},
+    {
+    question: "Kdo vyhrál nejvíce MS?",
+    "correct answer": 'Brazílie',
+    A: 'Brazílie',
+    B: 'Německo',
+    C: 'Anglie',
+    D: 'Argentina'
+},
+    {
+    question: "Kdo vyhrál první MS?",
+    "correct answer": 'Uruguay',
+    A: 'Francie',
+    B: 'Itálie',
+    C: 'Jugoslávie',
+    D: 'Uruguay'
+},
+    {
+    question: "Kdo je nejlepším střelcem MS?",
+    "correct answer": 'Miroslav Klose',
+    A: 'Ronaldo',
+    B: 'Gerd Muller',
+    C: 'Pelé',
+    D: 'Miroslav Klose'
+},
+    {
+    question: "Kolikrát se Československo probojovalo do finále MS?",
+    "correct answer": 2,
+    A: 0,
+    B: 1,
+    C: 2,
+    D: 3
+},
+    {
+    question: "Který hráč dál nejvíce branek během jednoho MS?",
+    "correct answer": 'Just Fontaine',
+    A: 'Ademir (1950)',
+    B: 'Sándor Kocsis (1954)',
+    C: 'Just Fontaine (1958)',
+    D: 'Gerd Muller (1970)'
+}
+]
