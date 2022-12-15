0c3d63734639b86db9a439f4918e7bba14d2fc99
Renzo R. Gelmetti <renzo.gelmetti@seznam.cz>
2022-12-15 01:21:12 +0100

    separated the deck of questions into its own js file to export

--- main.js
+++ main.js
@@ -1,61 +1,4 @@
-const DB = [
-    {
-    question: "Kdo vyhrál MS v roce 2006?",
-    "correct answer": 'Itálie',
-    A: 'Francie',
-    B: 'Itálie',
-    C: 'Anglie',
-    D: 'Argentina'
-},
-    {
-    question: "Kde se hrálo MS v roce 2002?",
-    "correct answer": 'V Japonsku a Koreji',
-    A: 'V Brazílii',
-    B: 'V USA',
-    C: 'V Japonsku a Koreji',
-    D: 'V Itálii'
-},
-    {
-    question: "Kdo vyhrál nejvíce MS?",
-    "correct answer": 'Brazílie',
-    A: 'Brazílie',
-    B: 'Německo',
-    C: 'Anglie',
-    D: 'Argentina'
-},
-    {
-    question: "Kdo vyhrál první MS?",
-    "correct answer": 'Uruguay',
-    A: 'Francie',
-    B: 'Itálie',
-    C: 'Jugoslávie',
-    D: 'Uruguay'
-},
-    {
-    question: "Kdo je nejlepším střelcem MS?",
-    "correct answer": 'Miroslav Klose',
-    A: 'Ronaldo',
-    B: 'Gerd Muller',
-    C: 'Pelé',
-    D: 'Miroslav Klose'
-},
-    {
-    question: "Kolikrát se Československo probojovalo do finále MS?",
-    "correct answer": 2,
-    A: 0,
-    B: 1,
-    C: 2,
-    D: 3
-},
-    {
-    question: "Který hráč dál nejvíce branek během jednoho MS?",
-    "correct answer": 'Just Fontaine',
-    A: 'Ademir (1950)',
-    B: 'Sándor Kocsis (1954)',
-    C: 'Just Fontaine (1958)',
-    D: 'Gerd Muller (1970)'
-}
-]
+import { DB } from "./database.js"
 
 const container = document.querySelector('.container')
 const answers = document.querySelectorAll('li')
@@ -73,7 +16,6 @@ startGame()
 
 function startGame() {
     randomQuestion = DB[Math.floor(Math.random() * DB.length)]
-    console.log(randomQuestion)
     questionElement.textContent = `${randomQuestion.question}`
     answerAElement.textContent = `A: ${randomQuestion.A}`
     answerBElement.textContent = `B: ${randomQuestion.B}`
@@ -89,8 +31,6 @@ function startGame() {
 
 function handleClick(e) {
     const answer = e.target.innerText.slice(3)
-    console.log(answer)
-    console.log(randomQuestion["correct answer"])
     message.innerHTML = answer == randomQuestion["correct answer"] ? 'You are correct!' : 'You are wrong!'
     restartButton()
 }
